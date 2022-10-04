cfc4855005b2bea8364c9dc84bcbb5ee4611d11c
Mark Iarantsev <markyarantsev@yahoo.com>
2022-10-04 09:58:58 +0500

    fixed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("006"))
+                .body("data", equalTo("007"))
         ;
     }
 }
